8c5d4e9e0337a58c9dc830ff9a2264176ee176e7
Mukhammed <arystaanbeek@gmail.com>
2023-09-26 14:57:42 +0600

    commit: update task2

---
+++ ConsoleAppModule01Task02/ConsoleAppModule01Task02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleAppModule01Task02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number (to finish typing, press Enter): ");
+            int sum = 0;
+            bool isEnteringNumbers = true;
+
+            while (isEnteringNumbers)
+            {
+                string input = Console.ReadLine();
+
+                if (input.ToLower() == "")
+                {
+                    isEnteringNumbers = false;
+                }
+                else if (int.TryParse(input, out int number)) 
+                {
+                    sum += number;
+                }
+                else
+                {
+                    Console.Write("Error! Please enter a number: ");
+                }
+            }
+
+            Console.WriteLine($"Sum of numbers: {sum}");
+            Console.ReadKey();
+        }
+    }
+}
