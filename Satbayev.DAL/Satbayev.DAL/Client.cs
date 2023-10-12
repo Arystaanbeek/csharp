d5d01a35f44d4aa7d04b0f88f945897e39a2cb17
Mukhammed <arystaanbeek@gmail.com>
2023-10-12 20:24:54 +0600

    add auth and register "

---
+++ Satbayev.DAL/Satbayev.DAL/Client.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Satbayev.DAL
+{
+    public class Client
+    {
+        public long Id { get; set; }
+        public DateTime Bday { get; set; }
+        public int GetAge
+        {
+            get
+            {
+                return DateTime.Now.Year - Bday.Year;
+            }
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string ShortName 
+        { 
+            get
+                {
+                if(string.IsNullOrEmpty(LastName))
+                {
+                    return string.Format("{0} {1}.", FirstName, LastName);
+                }
+                else
+                    return string.Format("{0} {1}.{2}.", 
+                        FirstName, MiddleName[0], LastName[0]);
+                }
+            }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+
+        public Address Address { get; set; }
+    }
+}
---
+++ Satbayev.DAL/Satbayev.DAL/repositoryClient.cs
@@ -0,0 +1,66 @@
+﻿using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Satbayev.DAL
+{
+    public class repositoryClient
+    {
+        private string path;
+
+        public repositoryClient(string path)
+        {
+            this.path = path;
+        }
+        public bool CreateClient(Client client)
+        {
+           
+                using (var db = new LiteDatabase(path))
+                {
+                    var clients = db.GetCollection<Client>("Client");
+                    clients.Insert(client);
+                }
+                return true;
+            
+            
+            
+        }
+        public Client GetClient(string email, string password) 
+        {
+            try
+            {
+                using (var db = new LiteDatabase(path))
+                {
+                    return db.GetCollection<Client>("Client")
+                        .FindAll().First(f => f.Email == email & f.Password == password);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+        }
+        public Client GetClient(int Id)
+        {
+            try
+            {
+                using (var db = new LiteDatabase(path))
+                {
+                    return db.GetCollection<Client>("Client")
+                        .FindAll().First(f => f.Id == Id);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+        }
+    }
+}
